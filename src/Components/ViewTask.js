8a2309ff890291bad207d0bee1271e0791d7dae7
IannohM <iannohlasty@gmail.com>
2025-01-31 15:39:06 +0300

    rectified the mistakes in all files

--- src/Components/ViewTask.js
+++ src/Components/ViewTask.js
@@ -1,15 +1,32 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom";
 import FetchData from "./FetchData";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import axios from "axios";
 
 const ViewTasks = () => {
     const { id } = useParams()
-    const { List: blogs } = FetchData('http://localhost:4000/blogs/' + id);
+    const { List: tasks } = FetchData('http://localhost:4000/tasks/' + id);
+    const history = useHistory();
+
+    const handleDelete = (e) => {
+        e.preventDefault();
+
+        axios.delete('http://localhost:4000/tasks/' + id)
+            .then(response => {
+                alert(`Task deleted successfully`);
+                history.push('/');
+            })
+    }
+
     return (
         <div>
-            {blogs && (
+            {tasks && (
                 <article>
-                    <h2>{blogs.date}</h2>
-                    <p>{blogs.list}</p>
+                    <h2>{tasks.date}</h2>
+                    <p>{tasks.time}</p>
+                    <p>{tasks.tasks}</p>
+                    <Button onClick={handleDelete} variant="danger">Delete</Button>
                 </article>
             )}
         </div>
